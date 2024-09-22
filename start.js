1e0a83e38260abd8da3c2247cd1d7741c296bed6
MelikaEl <melika.melika8@gmail.com>
2024-09-22 13:03:22 +0330

    shorter format for get request

--- start.js
+++ start.js
@@ -1,14 +1,21 @@
 // GET REQUEST
 function getTodos() {
-  axios({
-    method:'get',
-    url: 'https://jsonplaceholder.typicode.com/todos',
-    params:{
-      _limit:5
-    }
-  })
-  .then(res=>showOutput(res))
-  .catch(err=>console.log.error(err))
+ 
+  // axios({
+  //   method:'get',
+  //   url: 'https://jsonplaceholder.typicode.com/todos',
+  //   params:{
+  //     _limit:5
+  //   }
+  // })
+  // .then(res=>showOutput(res))
+  // .catch(err=>console.log.error(err))
+
+ axios.get('https://jsonplaceholder.typicode.com/todos',{
+  params:{_limit:5}
+ })
+ .then(res=>showOutput(res))
+ .catch(err=>console.error(err))
 }
 
 // POST REQUEST
