218e39330860416661845b3fa0e2d10d9a2f7b6f
MelikaEl <melika.melika8@gmail.com>
2024-09-23 10:32:20 +0330

    put and patch methods

--- start.js
+++ start.js
@@ -69,9 +69,22 @@ function addTodo() {
   // }
 }
 
-// PUT/PATCH REQUEST
+// PUT/PATCH REQUEST: For PUT/PATCH we need to indicate id too like this (id=1): /posts/1    
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+// axios.put('https://jsonplaceholder.typicode.com/todos/1',{
+//   title: 'Updated Todo',
+//   completed: true
+// })
+// .then(res=>showOutput(res))
+// .catch(err=>console.error(err));
+
+
+axios.patch('https://jsonplaceholder.typicode.com/todos/1',{
+  title: 'Updated Todo',
+  completed: true
+})
+.then(res=>showOutput(res))
+.catch(err=>console.error(err));
 }
 
 // DELETE REQUEST
