aeaafb8636b6b22aed50ea22c8a89c17ab7775b2
MelikaEl <melika.melika8@gmail.com>
2024-09-24 12:01:25 +0330

    axios global

--- start.js
+++ start.js
@@ -1,6 +1,9 @@
+//AXIOS GLOBALS
+axios.defaults.headers.common["X-Auth-Token"] =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
+
 // GET REQUEST
 function getTodos() {
- 
   // axios({
   //   method:'get',
   //   url: 'https://jsonplaceholder.typicode.com/todos',
@@ -11,37 +14,25 @@ function getTodos() {
   // .then(res=>showOutput(res))
   // .catch(err=>console.log.error(err))
 
+  //this is the shorter format of the above code (eliminate url object):
+  //  axios.get('https://jsonplaceholder.typicode.com/todos',{
+  //   params:{_limit:5}
+  //  })
+  //  .then(res=>showOutput(res))
+  //  .catch(err=>console.error(err))
 
+  //this is even more shorter format of the above code (eliminate params object):
+  // axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
+  // .then(res=>showOutput(res))
+  // .catch(err=>console.error(err))
 
-
-//this is the shorter format of the above code (eliminate url object): 
-//  axios.get('https://jsonplaceholder.typicode.com/todos',{
-//   params:{_limit:5}
-//  })
-//  .then(res=>showOutput(res))
-//  .catch(err=>console.error(err))
-
-
-
-
-//this is even more shorter format of the above code (eliminate params object): 
-// axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-// .then(res=>showOutput(res))
-// .catch(err=>console.error(err))
-
-
-
-
-axios('https://jsonplaceholder.typicode.com/todos?_limit=5')// when we run axios without any method, it means the get method by default
-.then(res=>showOutput(res))
-.catch(err=>console.error(err))
-
+  axios("https://jsonplaceholder.typicode.com/todos?_limit=5") // when we run axios without any method, it means the get method by default
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // POST REQUEST
 function addTodo() {
-
-
   // axios({
   //   method:'post',
   //   url: 'https://jsonplaceholder.typicode.com/todos',
@@ -51,17 +42,15 @@ function addTodo() {
   //   }
   // })
 
-
-
-
   //the shorter format opf the post method
-  axios.post('https://jsonplaceholder.typicode.com/todos',{
-    title: 'New Todo',
-    completed:false
-  })
-  .then(res=>showOutput(res))
-  .catch(err=>console.error(err));
-  //the data that is generated after we run the post method (the id is 201 because it generates 200 numbers of data): 
+  axios
+    .post("https://jsonplaceholder.typicode.com/todos", {
+      title: "New Todo",
+      completed: false,
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
+  //the data that is generated after we run the post method (the id is 201 because it generates 200 numbers of data):
   // {
   //   "title": "New Todo",
   //   "completed": false,
@@ -69,29 +58,30 @@ function addTodo() {
   // }
 }
 
-// PUT/PATCH REQUEST: For PUT/PATCH we need to indicate id too like this (id=1): /posts/1    
+// PUT/PATCH REQUEST: For PUT/PATCH we need to indicate id too like this (id=1): /posts/1
 function updateTodo() {
-// axios.put('https://jsonplaceholder.typicode.com/todos/1',{
-//   title: 'Updated Todo',
-//   completed: true
-// })
-// .then(res=>showOutput(res))
-// .catch(err=>console.error(err));
+  // axios.put('https://jsonplaceholder.typicode.com/todos/1',{
+  //   title: 'Updated Todo',
+  //   completed: true
+  // })
+  // .then(res=>showOutput(res))
+  // .catch(err=>console.error(err));
 
-
-axios.patch('https://jsonplaceholder.typicode.com/todos/1',{
-  title: 'Updated Todo',
-  completed: true
-})
-.then(res=>showOutput(res))
-.catch(err=>console.error(err));
+  axios
+    .patch("https://jsonplaceholder.typicode.com/todos/1", {
+      title: "Updated Todo",
+      completed: true,
+    })
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
- axios.delete('https://jsonplaceholder.typicode.com/todos/1')
- .then(res=>showOutput(res))
- .catch(err=>console.error(err));
+  axios
+    .delete("https://jsonplaceholder.typicode.com/todos/1")
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // SIMULTANEOUS DATA : with axios.all
@@ -107,74 +97,77 @@ function getData() {
   // })
   // .catch(err=>console.error(err));
 
-
   //for show the name for response
-  axios.all([
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5'),
-    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5'),
-  ])
-  .then(axios.spread((todos,posts)=> showOutput(posts)))
-  .catch(err=>console.error(err));
+  axios
+    .all([
+      axios.get("https://jsonplaceholder.typicode.com/todos?_limit=5"),
+      axios.get("https://jsonplaceholder.typicode.com/posts?_limit=5"),
+    ])
+    .then(axios.spread((todos, posts) => showOutput(posts)))
+    .catch((err) => console.error(err));
 }
-
 
 // CUSTOM HEADERS
 function customHeaders() {
-  const config={
-    headers:{
-        'Content-Type':'application/json',
-        Authorization:'sometoken'
-    }
-  };
-  axios.post('https://jsonplaceholder.typicode.com/todos',
-    {
-      title:'New Todo',
-      completed:false
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "sometoken",
     },
-    config
-  )
-  .then(res=>showOutput(res))
-  .catch(err=>console.error(err))
+  };
+  axios
+    .post(
+      "https://jsonplaceholder.typicode.com/todos",
+      {
+        title: "New Todo",
+        completed: false,
+      },
+      config
+    )
+    .then((res) => showOutput(res))
+    .catch((err) => console.error(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-const options={
-  method:'post',
-  url:'https://jsonplaceholder.typicode.com/todos',
-  data:{
-    title:'Hello World'
-  },
-  transformResponse: axios.defaults.transformResponse.concat(data=>{
-    data.title=data.title.toUpperCase();
-    return data;
-  })
-}
-axios(options).then(res=>showOutput(res))
+  const options = {
+    method: "post",
+    url: "https://jsonplaceholder.typicode.com/todos",
+    data: {
+      title: "Hello World",
+    },
+    transformResponse: axios.defaults.transformResponse.concat((data) => {
+      data.title = data.title.toUpperCase();
+      return data;
+    }),
+  };
+  axios(options).then((res) => showOutput(res));
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  console.log("Error Handling");
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  console.log("Cancel Token");
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
 axios.interceptors.request.use(
-  config=>{
+  (config) => {
     console.log(
-      `${config.method.toUpperCase()} request send to ${config.url} at ${new Date().getTime()}`
-    )
+      `${config.method.toUpperCase()} request send to ${
+        config.url
+      } at ${new Date().getTime()}`
+    );
     return config;
   },
-  error => {
-    return Promise.reject(error)
+  (error) => {
+    return Promise.reject(error);
   }
-)
+);
 /* 
 axios.interceptors.request.use(...):
 
@@ -365,19 +358,17 @@ config.url: The URL of the request.
 new Date().getTime(): The current timestamp (not a property of config, but a separate expression).
 */
 
-
-
-
 // AXIOS INSTANCES
 
 // Show output in browser
-function showOutput(res) {// The function showOutput takes a single parameter res, which is expected to be an object containing information about a response.
- //The function updates the inner HTML of an element with the ID res.
- //${res.status}: This section creates a Bootstrap card with a header that displays the status of the response. The res.status value is interpolated into the HTML string.
- //${JSON.stringify(res.headers, null, 2)}: This section creates another Bootstrap card with the header "Headers". The headers of the response, stored in res.headers, are converted to a JSON string with JSON.stringify and displayed in a <pre> block for better readability.
- //${JSON.stringify(res.data, null, 2)}: This section creates a card with the header "Data". The data of the response, stored in res.data, is also converted to a JSON string and displayed in a <pre> block.
- //${JSON.stringify(res.config, null, 2)}: This section creates a card with the header "Config". The configuration of the response, stored in res.config, is similarly converted to a JSON string and displayed.
- document.getElementById('res').innerHTML = `
+function showOutput(res) {
+  // The function showOutput takes a single parameter res, which is expected to be an object containing information about a response.
+  //The function updates the inner HTML of an element with the ID res.
+  //${res.status}: This section creates a Bootstrap card with a header that displays the status of the response. The res.status value is interpolated into the HTML string.
+  //${JSON.stringify(res.headers, null, 2)}: This section creates another Bootstrap card with the header "Headers". The headers of the response, stored in res.headers, are converted to a JSON string with JSON.stringify and displayed in a <pre> block for better readability.
+  //${JSON.stringify(res.data, null, 2)}: This section creates a card with the header "Data". The data of the response, stored in res.data, is also converted to a JSON string and displayed in a <pre> block.
+  //${JSON.stringify(res.config, null, 2)}: This section creates a card with the header "Config". The configuration of the response, stored in res.config, is similarly converted to a JSON string and displayed.
+  document.getElementById("res").innerHTML = `
   <div class="card card-body mb-4">
     <h5>Status: ${res.status}</h5>
   </div>
@@ -409,17 +400,17 @@ function showOutput(res) {// The function showOutput takes a single parameter re
     </div>
   </div>
 `;
-}//The showOutput function takes a response object (res) and updates the HTML content of an element with the ID res to display the status, headers, data, and configuration of the response. It uses Bootstrap's card components for layout and JSON stringification for readability. If you'd like a visual representation of a response, you can toggle on image generation mode in the settings panel.
+} //The showOutput function takes a response object (res) and updates the HTML content of an element with the ID res to display the status, headers, data, and configuration of the response. It uses Bootstrap's card components for layout and JSON stringification for readability. If you'd like a visual representation of a response, you can toggle on image generation mode in the settings panel.
 
 // Event listeners
-document.getElementById('get').addEventListener('click', getTodos);
-document.getElementById('post').addEventListener('click', addTodo);
-document.getElementById('update').addEventListener('click', updateTodo);
-document.getElementById('delete').addEventListener('click', removeTodo);
-document.getElementById('sim').addEventListener('click', getData);
-document.getElementById('headers').addEventListener('click', customHeaders);
+document.getElementById("get").addEventListener("click", getTodos);
+document.getElementById("post").addEventListener("click", addTodo);
+document.getElementById("update").addEventListener("click", updateTodo);
+document.getElementById("delete").addEventListener("click", removeTodo);
+document.getElementById("sim").addEventListener("click", getData);
+document.getElementById("headers").addEventListener("click", customHeaders);
 document
-  .getElementById('transform')
-  .addEventListener('click', transformResponse);
-document.getElementById('error').addEventListener('click', errorHandling);
-document.getElementById('cancel').addEventListener('click', cancelToken);
+  .getElementById("transform")
+  .addEventListener("click", transformResponse);
+document.getElementById("error").addEventListener("click", errorHandling);
+document.getElementById("cancel").addEventListener("click", cancelToken);
